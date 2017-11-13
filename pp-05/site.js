5e06d5fb00803ed2e0593c56fe09d3fcd0cf0e17
Alex <anicoara@hawk.iit.edu>
2017-11-12 23:45:45 -0600

    Added self executing function with some variables

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,1 +1,12 @@
-\
+$.noConflict();
+
+// Re-introduce the dollar sign scope to self executing function
+(function($) {
+  // Variables
+  var doorbellSound = new Audio('media/doorbell.mp3');
+  var auplayer = document.getElementById("doorbell-auplayer");
+  var message = document.getElementById("status");
+  var doorbell;
+
+  
+})(jQuery);
